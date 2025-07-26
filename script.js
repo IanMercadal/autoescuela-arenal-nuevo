ab9334983631416ffd8bffd6cb92bb243c3e6b86
Ian Mercadal <ianmercadalf02@gmail.com>
2025-07-26 16:29:16 +0200

    feat: v.1
    MVP finished
    
    MVP finished

---
+++ script.js
@@ -0,0 +1,13 @@
+function activeLink(link) {
+    
+}
+
+function openMobileNav() {
+    let mobileNav = document.querySelector('.navigation-mobile');
+    mobileNav.classList.remove('hidden');
+}
+
+function closeMobileNav() {
+    let mobileNav = document.querySelector('.navigation-mobile');
+    mobileNav.classList.add('hidden');
+}
